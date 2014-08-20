a26311280de26798e01c1f8e18f8e5c30113a717
trs-mark <trs.markt@gmail.com>
2014-08-20 14:17:57 +0800

    test A -> 1-10, clues are written on comments

--- Resources/iphone/alloy/controllers/index.js
+++ Resources/iphone/alloy/controllers/index.js
@@ -2,65 +2,12 @@ function Controller() {
     function init() {
         setAddTaskButton();
         $.index.open();
-        getToDo();
         getDone();
     }
     function setAddTaskButton() {
-        var btnAddTask = Titanium.UI.createButton({
+        Titanium.UI.createButton({
             title: "Add Task"
         });
-        btnAddTask.addEventListener("click", addTask);
-        $.win1.rightNavButton = btnAddTask;
-    }
-    function addTask() {
-        var dialog = Ti.UI.createAlertDialog({
-            title: "Type your task here.",
-            style: Ti.UI.iPhone.AlertDialogStyle.PLAIN_TEXT_INPUT,
-            buttonNames: [ "Ok", "Cancel" ],
-            cancel: 1
-        });
-        dialog.addEventListener("click", function(dialog_evt) {
-            if (0 === dialog_evt.index) if (dialog_evt.text.trim().length > 0) {
-                saveTask(dialog_evt.text.trim());
-                getToDo();
-            } else Titanium.UI.createAlertDialog({
-                title: "Task not added",
-                message: "Task cannot be empty."
-            }).show();
-        });
-        dialog.show();
-    }
-    function saveTask(task) {
-        tasksCollection = Alloy.Collections.instance("tasks");
-        tasksCollection.fetch();
-        var taskModel = Alloy.createModel("tasks", {
-            title: task,
-            status: 0
-        });
-        tasksCollection.add(taskModel);
-        taskModel.save();
-    }
-    function getToDo() {
-        tasksCollection = Alloy.Collections.instance("tasks");
-        var sql = "SELECT * FROM " + table + " WHERE status=0";
-        tasksCollection.fetch({
-            query: sql
-        });
-        var tasksArr = [];
-        for (var i = 0; tasksCollection.length > i; i++) {
-            var task = tasksCollection.at(i);
-            var id = task.get("id");
-            var title = task.get("title");
-            task.get("status");
-            var row = Titanium.UI.createTableViewRow({
-                title: title,
-                c_id: id,
-                hasChild: true
-            });
-            row.addEventListener("click", todoRowFunction);
-            tasksArr.push(row);
-        }
-        $.tblToDo.setData(tasksArr);
     }
     function getDone() {
         tasksCollection = Alloy.Collections.instance("tasks");
@@ -71,35 +18,17 @@ function Controller() {
         var tasksArr = [];
         for (var i = 0; tasksCollection.length > i; i++) {
             var task = tasksCollection.at(i);
-            var id = task.get("id");
+            task.get("id");
             var title = task.get("title");
             task.get("status");
             var row = Titanium.UI.createTableViewRow({
                 title: title,
-                c_id: id,
                 hasCheck: true
             });
             row.addEventListener("click", doneRowFunction);
             tasksArr.push(row);
         }
         $.tblDone.setData(tasksArr);
-    }
-    function todoRowFunction(row_evt) {
-        var dialog = Titanium.UI.createAlertDialog({
-            title: row_evt.source.title,
-            message: "What do you want to do?",
-            buttonNames: [ "Mark As Done", "Cancel" ],
-            cancel: 1
-        });
-        dialog.addEventListener("click", function(dialog_evt) {
-            if (0 === dialog_evt.index) {
-                setItemAsDone(row_evt.source.c_id);
-                getToDo();
-                getDone();
-            }
-        });
-        dialog.show();
-        return false;
     }
     function doneRowFunction(row_evt) {
         var dialog = Titanium.UI.createAlertDialog({
@@ -109,26 +38,9 @@ function Controller() {
             cancel: 1
         });
         dialog.addEventListener("click", function(dialog_evt) {
-            if (0 === dialog_evt.index) {
-                removeItem(row_evt.source.c_id);
-                getDone();
-            }
+            0 === dialog_evt.index && removeItem(row_evt.source.c_id);
         });
         dialog.show();
-        return false;
-    }
-    function setItemAsDone(id) {
-        tasksCollection = Alloy.Collections.instance("tasks");
-        var sql = "SELECT * FROM " + table + " WHERE id=" + id;
-        tasksCollection.fetch({
-            query: sql
-        });
-        if (tasksCollection.length > 0) {
-            var model = tasksCollection.at(0);
-            model.set({
-                status: 1
-            }).save();
-        }
         return false;
     }
     function removeItem(id) {
